41d791201094958a205843b433321aeb0668f247
Medeu Ametay <medeu_ametay@epam.com>
2025-08-25 23:56:44 +0500

    Refactored services via task requirements with jpa/orm

--- src/main/java/com/epam/crm/Main.java
+++ src/main/java/com/epam/crm/Main.java
@@ -4,21 +4,11 @@ import com.epam.crm.config.ApplicationConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.util.HashMap;
-
 
 @Slf4j
 public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(ApplicationConfig.class);
         ctx.registerShutdownHook();
-
-        log.info("Application started. Context id = {}", ctx.getId());
-
-        log.debug("Users: {}", ctx.getBean("users", HashMap.class));
-        log.debug("trainees: {}", ctx.getBean("trainees", HashMap.class));
-        log.debug("trainers: {}", ctx.getBean("trainers", HashMap.class));
-        log.debug("training types: {}", ctx.getBean("trainingTypes", HashMap.class));
-        log.debug("trainings: {}", ctx.getBean("trainings", HashMap.class));
     }
 }
