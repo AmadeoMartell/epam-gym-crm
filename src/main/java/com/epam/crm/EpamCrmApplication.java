fb385608830ad93cdc9990ef504775efe982a966
Medeu Ametay <medeu_ametay@epam.com>
2025-11-04 00:22:27 +0500

    Add eureka client dependency

--- src/main/java/com/epam/crm/EpamCrmApplication.java
+++ src/main/java/com/epam/crm/EpamCrmApplication.java
@@ -2,12 +2,14 @@ package com.epam.crm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableDiscoveryClient
 @Profile("dev")
 public class EpamCrmApplication {
     public static void main(String[] args) {
